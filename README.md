8167fc45b35b8c2d2540c9915598a946671d66e1
m <m@m.mmm>
2021-10-01 21:08:55 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # text-generator
-Es un generador de textos y experimentos relacionados
+Es un generador de textos y experimentos relacionados.
+
+## API Analizadora de sentimientos
+*/sentiment/?text=texto*
+<br>
+Analiza el sentimiento del "texto" como positivo o negativo.
+El texto debe ser proporcionado en inglés.
+
--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 from transformers import pipeline
+from fastapi import FastAPI
 
+app = FastAPI()
 classifier = pipeline("sentiment-analysis")
-print(classifier("You suck motherfucker!"))
+
+@app.get("/sentiment/")
+def root(text="I'm happy!"):
+    msg = classifier(text)
+    return {
+        "message": text,
+        "label": msg[0]["label"],
+        "score": msg[0]["score"]
+    }
